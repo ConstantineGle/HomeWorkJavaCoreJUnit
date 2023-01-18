72be36b5e85b24bf5d79edd7023ed14470006c81
glebov <constantine.glebov@gmail.com>
2023-01-18 15:19:29 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+
+        int d = calc.multiply.apply(4, 5);
+        int e = calc.abs.apply(-6);
+
+        calc.println.accept(a);
+        calc.println.accept(b);
+        calc.println.accept(d);
+        calc.println.accept(e);
+
+    }
+}
+
+@FunctionalInterface
+interface Supplier<C> {
+    Calculator get();
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CalculatorTest {
+
+    Calculator sut;
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("@beforeAll");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        sut = new Calculator();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("@afterAll");
+    }
+
+    @Test
+    public void plus() {
+        int a = 2, b = 3, expected = 5;
+        int actual = sut.plus.apply(a, b);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void divide() {
+        int a = 8, b = 0;
+        Assertions.assertThrows(ArithmeticException.class, () -> sut.divide.apply(a, b));
+    }
+
+    @Test
+    public void isPositive() {
+        int a = 5;
+        boolean actual = sut.isPositive.test(a);
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void isPositiveHamcrest() {
+
+    }
+}
